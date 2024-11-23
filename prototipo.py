93b1be20c8826929384ca6803b5517f7866a4465
Sambek99 <bgaona@espol.edu.ec>
2024-11-22 19:18:28 -0500

    SE TERMINA
    SE AGREGA LO QUE FALTA

--- prototipo.py
+++ prototipo.py
@@ -1,5 +1,6 @@
 import random as rd
 import time
+import datetime as dt
 propietarios_dict = {
     "22 15 ": "Juan Pérez",
     "18 10 ": "María González",
@@ -23,6 +24,19 @@ visitantes = {
     "0910111213": ["Jorge Castro", "MZ-6", "V-22", "2024-11-14"],
     "1011121314": ["Elena Morales", "MZ-1", "V-30", "2024-11-13"]
 }
+preautorizado = {
+    "0102030405": ["Juan Pérez", "MZ-10", "V-15", "2024-11-22", "2024-11-24"],
+    "0203040506": ["María González", "MZ-5", "V-8", "2024-11-21", "2024-11-23"],
+    "0304050607": ["Carlos Martínez", "MZ-3", "V-20", "2024-11-20", "2024-11-22"],
+    "0405060708": ["Ana López", "MZ-7", "V-14", "2024-11-19", "2024-11-21"],
+    "0506070809": ["Luis Ramírez", "MZ-12", "V-5", "2024-11-18", "2024-11-21"],
+    "0607080910": ["Sofía Torres", "MZ-8", "V-10", "2024-11-17", "2024-11-18"],
+    "0708091011": ["Miguel Chávez", "MZ-9", "V-25", "2024-11-16", "2024-11-18"],
+    "0809101112": ["Laura Fernández", "MZ-4", "V-19", "2024-11-15", "2024-11-18"],
+    "0910111213": ["Jorge Castro", "MZ-6", "V-22", "2024-11-14", "2024-11-16"],
+    "1011121314": ["Elena Morales", "MZ-1", "V-30", "2024-11-13", "2024-11-15"]
+}
+
 lista_negra = {
     "0102030405": "Juan Pérez",
     "0203040506": "María González",
@@ -77,13 +91,44 @@ while valido:
         while (isinstance(opv, int) and opv not in [1,2,3,4]):
             opr = int(input("Escoja una opción: "))
         if (opr == 1):
-            break
+            nombre = input("Ingrese el nombre: ")
+            cedula = input("Ingrese la cédula: ")
+            fecha=  input("Ingrese la fecha inicio AAAA/MM/DD: ")
+            fechaFin =  input("Ingrese la fecha fin AAAA/MM/DD: ")
+            mz = rd.randint(1,25)
+            villa = rd.randint(1,29)
+            datos  = ["{}".format(nombre), "MZ-{}".format(mz), "V-{}".format(villa), "{}".format(fecha), "{}".format(fechaFin)]
+            preautorizado.update({cedula: datos})
+            print("SE GENERO LA PREAUTPRIZACION PARA LA PERSONA: {}\n CON FECHAS\n INICIO:{}\nFIN: {}".format(nombre,fecha,fechaFin))
+            valido = False
         elif (opr== 2):
-            break
+            print("HISTORIAL DE VISITAS: ")
+            for i in visitantes.keys():
+                    print("{}: {}".format(i," ".join(list[i])))
+            valido = False
         elif (opr == 3):
-            break
+            nombre = input("Ingrese el nombre: ")
+            cedula = input("Ingrese la cédula: ")
+            fecha= dt.now() 
+            fechaFin =  fecha + dt.timedelta(days=1)
+            qr = hash(nombre+cedula+str(fecha)+str(fechaFin))
+            print("SE GENERO EL QR PARA LA PERSONA: {}\nCODIGO: {}".format(nombre,qr))
+            valido = False
         elif (opr == 4):
-            break
+            print("1 MOSTRAR LISTA NEGRA \n2 AGREGAR A LISTA NEGRA")
+            opl = int(input("Escoja una opción: "))
+            while (isinstance(opv, int) and opv not in [1,2,3,4]):
+                opl = int(input("Escoja una opción: "))
+            if (opr == 1):
+                for i in lista_negra.keys():
+                    print("{}: {}".format(i,list[i]))
+                valido = False
+            elif (opr== 2):
+                nombre = input("Ingrese el nombre: ")
+                cedula = input("Ingrese la cédula: ")
+                lista_negra.update({cedula:nombre})
+                print("SE AGREGO A {} A LA LISTA NEGRA".format(nombre))
+                valido = False
     elif (op == 3):
         print()
         print("Bienvenido")
@@ -102,6 +147,7 @@ while valido:
                 print("SE REGISTRA A {} CON INGRESO A LA CASA {}".format(nombre,casa))
             else: 
                 print("PERSONA EN LISTA NEGRA, NEGAR INGRESO")
+            valido = False
         elif (opg== 2):
             mz = input("A que manzana se dirige: ")
             villa = input("A que villa se dirige: ")
