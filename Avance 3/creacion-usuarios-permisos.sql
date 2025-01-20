e3c251e0357ebcbcdb30d5406c8fab2d09fb9877
jorgelet <jddelcampom@gmail.com>
2025-01-19 19:15:39 -0500

    add: grant permissions for new users in SQL script

--- Avance 3/creacion-usuarios-permisos.sql
+++ Avance 3/creacion-usuarios-permisos.sql
@@ -1,6 +1,11 @@
 CREATE USER 'propietario' IDENTIFIED BY 'Contrapropietario';
+GRANT SELECT, INSERT ON ciudadela.propietario TO 'propietario';
+GRANT SELECT, INSERT ON ciudadela.casa TO 'propietario';
+GRANT SELECT ON ciudadela.HistorialPreAutorizados TO 'propietario';
 
 CREATE USER 'visitante' IDENTIFIED BY 'ContrasenaVisitante';
+GRANT SELECT, INSERT ON ciudadela.visitante TO 'visitante';
+GRANT SELECT ON ciudadela.lista_negra TO 'visitante';
 
 CREATE USER 'residente' IDENTIFIED BY 'ContraResidente';
 GRANT EXECUTE ON ciudadela.insertar_y_generar_qr TO 'residente';
@@ -8,8 +13,10 @@ GRANT EXECUTE ON ciudadela.insertar_y_generar_autorizacion TO 'residente';
 
 CREATE USER 'guardia' IDENTIFIED BY 'ContraGuardia';
 GRANT SELECT, UPDATE ON ciudadela.autorizacionguardia TO 'guardia';
+GRANT SELECT, INSERT ON ciudadela.codigoqr TO 'guardia';
 
 CREATE USER 'administrador' IDENTIFIED BY 'Contramaster';
 GRANT ALL PRIVILEGES ON ciudadela.* TO 'administrador';
+GRANT SELECT ON ciudadela.HistorialDePagos TO 'administrador';
 
 FLUSH PRIVILEGES;
