53141cf3a347f5472fa131e25531adc14d62cf03
anzagood1 <anzagood@espol.edu.ec>
2025-01-29 08:27:16 -0500

    Update views.sql

--- Avance 3/views.sql
+++ Avance 3/views.sql
@@ -1,3 +1,4 @@
+use ciudadela;
 CREATE VIEW HistorialPreAutorizados AS
 Select ag.hora hora , ag.fecha fecha, v.nombre "Nombre visita", v.apellido "Apellido visita", ag.codigo_catastral "Codigo Catastral"
 FROM visitante v inner join autorizacionguardia ag on v.numero_de_cedula = ag.cedula_visitante 
@@ -12,7 +13,7 @@ ORDER BY pa.fecha_pago DESC;
 CREATE VIEW HistorialCodigoQR as
 SELECT  v.nombre "Nombre visitante" , v.apellido "Apellido visitante", qr.fecha_inicio, qr.fecha_fin, c.codigo_catastral
 from codigoqr qr inner join visitante v on v.numero_de_cedula = qr.cedula_visitante 
-inner join casa c on c.numero_de_cedula = qr.cedula_propietario;
+inner join casa c on c.numero_de_cedula = qr.cedula_propietario
 ORDER BY qr.fecha_inicio DESC;
 
 CREATE VIEW ListaNegra as
@@ -30,19 +31,20 @@ ORDER BY ag.fecha DESC;
 END//
 DELIMITER ;
 
-DELIMITER//
+DELIMITER //
 CREATE PROCEDURE obtenerHistorialCodigoQR(IN cedula_propietario char(10))
 BEGIN
 SELECT  v.nombre "Nombre visitante" , v.apellido "Apellido visitante", qr.fecha_inicio, qr.fecha_fin, c.codigo_catastral
 from codigoqr qr inner join visitante v on v.numero_de_cedula = qr.cedula_visitante 
-inner join casa c on c.numero_de_cedula = qr.cedula_propietario;
+inner join casa c on c.numero_de_cedula = qr.cedula_propietario
 WHERE c.numero_de_cedula = cedula_propietario
 ORDER BY qr.fecha_inicio DESC;
-END//
+END
+//
 DELIMITER ;
 
 
-DELIMITER//
+DELIMITER //
 CREATE PROCEDURE obtenerHistorialDePagos(IN cedula_propietario char(10))
 BEGIN 
 Select concat(a.nombre," ",a.apellido) "Nombre Administrador", concat(pr.nombre," ",pr.apellido) "Nombre Propietario", pa.fecha_pago Fecha, pa.monto Monto
@@ -56,7 +58,8 @@ DELIMITER //
 CREATE PROCEDURE obtenerListaNegra(IN codigo_catastral char(25))
 BEGIN
 Select v.numero_de_cedula "Numero de cedula", v.nombre "Nombre", v.apellido "Apellido"
-from lista_negra ln join visitante v using numero_de_cedula join casas c using codigo_catastral
-WHERE c.codigo_catastral = codigo_catastral
-END//
+from lista_negra ln join visitante v using (numero_de_cedula) join casas c using (codigo_catastral)
+WHERE c.codigo_catastral = codigo_catastral;
+END;
+//
 DELIMITER ;
